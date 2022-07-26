8f9882cc65ebc6aa455642ec892a78148075f73c
Samson Amaugo <sammychinedu2ky@gmail.com>
2022-07-26 13:45:19 +0100

    edit: position of text

--- NotificationService.cs
+++ NotificationService.cs
@@ -57,7 +57,7 @@ class NotificationService : IHostedService
                     {
                         var payload = new
                         {
-                            content = $"Title: **{movie.Title}**\nLink: **{movie.Link}**\nNo: **{counter++}**"
+                            content = $"No: **{counter++}**\nTitle: **{movie.Title}**\nLink: **{movie.Link}**"
                         };
                         
                         _sendClient.Send(payload);
